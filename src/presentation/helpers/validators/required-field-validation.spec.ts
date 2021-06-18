83cf3c5b01253f42f3891a3264dc6bbc833b378e
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-06-17 21:05:39 -0400

    test: ensure RequiredFieldValidation not return if validation success

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('Required Field', () => {
     const error = sut.validate({ name: 'any name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('should not return if validation success', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
 })
